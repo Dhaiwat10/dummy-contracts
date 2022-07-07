2a6149a5bb868ca77da237a2d5d3a16c4053387d
Dhaiwat Pandya <dhaiwatpandya@gmail.com>
2022-07-07 20:16:50 +0530

    init

---
+++ contracts/MyNFT.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract MyNFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    string public commonTokenURI;
+    uint256 public mintCost;
+
+    constructor(string memory _tokenURI, uint256 _mintCost)
+        ERC721("MyNFT", "NFT")
+    {
+        commonTokenURI = _tokenURI;
+        mintCost = _mintCost;
+    }
+
+    function mint(address _to) public payable returns (uint256) {
+        require(msg.value >= mintCost, "Min cost 0.001ETH");
+        uint256 newTokenId = _tokenIds.current();
+        _mint(_to, newTokenId);
+        _setTokenURI(newTokenId, commonTokenURI);
+        _tokenIds.increment();
+        return newTokenId;
+    }
+}
